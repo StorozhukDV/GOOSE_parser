eacd87b07351a959c4d107b4e2f5bd2ffffa1738
Storozhuk Danil <danilstorojuk@yandex.ru>
2022-11-11 00:41:11 +0300

    Update GoosePdu.java

--- src/main/java/Goose/GoosePdu.java
+++ src/main/java/Goose/GoosePdu.java
@@ -5,9 +5,5 @@ import lombok.Data;
 
 @Data
 public class GoosePdu {
-
-    //Здесь расспарсить мой  XML в структуру гуся, дальше когда буду парсить сам пакет, значения переменных записываю отуда сюда.
     private Dataset dataset;
-
-
 }
